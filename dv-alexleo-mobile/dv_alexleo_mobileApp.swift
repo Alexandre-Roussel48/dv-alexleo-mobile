d2cdf956bd8708ffac362b56d4fc93affb0a1273
dudewiththenose1 <lsauvage71@gmail.com>
2025-03-24 11:37:05 +0100

    init

---
+++ dv-alexleo-mobile/dv_alexleo_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  dv_alexleo_mobileApp.swift
+//  dv-alexleo-mobile
+//
+//  Created by etud on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct dv_alexleo_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
