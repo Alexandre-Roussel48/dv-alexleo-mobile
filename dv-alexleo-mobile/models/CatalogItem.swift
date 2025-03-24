ac5f3b415d30359445c826f6adb94a702f702c2b
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-03-24 10:53:41 +0100

    update gitignore

--- dv-alexleo-mobile/models/CatalogItem.swift
+++ dv-alexleo-mobile/models/CatalogItem.swift
@@ -1,7 +1,16 @@
-//
-//  CatalogItem.swift
-//  dv-alexleo-mobile
-//
-//  Created by alexandre.roussel03 on 24/03/2025.
-//
+import Foundation
 
+struct CatalogItem: Identifiable, Codable {
+    let id = UUID()
+    let unitPrice: Double
+    let quantity: Int
+    let gameName: String
+    let gameEditor: String
+    let sellerName: String
+    let sellerSurname: String
+
+    // Optional: Computed property for full seller name
+    var sellerFullName: String {
+        "\(sellerName) \(sellerSurname)"
+    }
+}
