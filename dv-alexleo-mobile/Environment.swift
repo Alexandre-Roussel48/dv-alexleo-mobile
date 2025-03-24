2e8872c9ac08319202bc0bc423e18fbd9c4447df
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-03-24 12:24:36 +0100

    enviroment

--- dv-alexleo-mobile/Environment.swift
+++ dv-alexleo-mobile/Environment.swift
@@ -5,3 +5,8 @@
 //  Created by alexandre.roussel03 on 24/03/2025.
 //
 
+import Foundation
+
+struct Environment {
+    static let baseURL: String = "https://dv-backend-quarkus.cluster-ig4.igpolytech.fr/api"
+}
